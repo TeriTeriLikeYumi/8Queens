b27e9940c669fd47a5ebf54c859a45e60cdd32ca
TeriTeriLikeYumi <tranquocthuan200@gmail.com>
2022-04-20 12:52:01 +0700

    Update Task D

--- 8Queens.py
+++ 8Queens.py
@@ -1,5 +1,7 @@
 from numpy import true_divide
 from pysat.solvers import Glucose3
+from pysat.solvers import Lingeling
+from copy import deepcopy
 SIZE = 8
 f1 = open("level01", "w")
 f2 = open("level02", "w")
@@ -16,10 +18,13 @@ def outputRowAtLeast1(f):
     """
     for i in range (SIZE):
         for j in range (SIZE):
+            #If it is the last element in the row, then don't add the "v"
             if (j != SIZE-1):
                 f.write("p[%d][%d] v " % (i, j))
             else:
                 f.write("p[%d][%d]" % (i, j))
+            
+            #Check whether it is the last element in the row
             if j == SIZE-1 and i != SIZE-1:
                 f.write("\n")
             elif j == SIZE-1 and i == SIZE-1:
@@ -32,7 +37,9 @@ def outputRowAtMost1(f):
     for i in range (SIZE):
         for j in range (SIZE-1):
             for k in range(j+1, SIZE):
-                f.write("not(p[%d][%d]) v not(p[%d][%d])\n" % (i, j, i, k))
+                #Restrict element in the row to be 1
+                f.write("not(p[%d][%d]) v not(p[%d][%d])" % (i, j, i, k))
+                f.write("\n")
 
 #ONE QUEEN IN A COLUMN
 def outputColumnAtLeast1(f):
@@ -41,10 +48,13 @@ def outputColumnAtLeast1(f):
     """
     for i in range (SIZE):
         for j in range (SIZE):
+            #If it is the last element in the column, then don't add the "v"
             if (j != SIZE-1):
                 f.write("p[%d][%d] v " % (j,i))
             else:
                 f.write("p[%d][%d]" % (j,i))
+                
+            #Check whether it is the last element in the column
             if j == SIZE-1 and i != SIZE-1:
                 f.write("\n")
             elif j == SIZE-1 and i == SIZE-1:
@@ -57,10 +67,9 @@ def outputColumnAtMost1(f):
     for i in range(SIZE):
         for j in range(SIZE-1):
             for k in range (j+1, SIZE):
-                f.write("not(p[%d][%d]) v not(p[%d][%d])" % (j,i,k,i))
-                if (i != SIZE-1 or j != SIZE-2 or k != SIZE-1):
-                    f.write("\n")
-    f.write("\n")
+                #Restrict element in the column to be 1
+                f.write("not(p[%d][%d]) v not(p[%d][%d])\n" % (j, i , k , i)) #Similar to the row just swap the i 
+    
 
 #NO MORE THAN 1 QUEEN IN THE LEFT DIAGONAL & RIGHT DIAGONAL
 def outputAtMostOneQueenInDiagonal(f):
@@ -115,12 +124,7 @@ filetext.close()
 #--------------------------------------------------
 #--------------------------------------------------
 #--------------------------------------------------
-#Task C
-
-from copy import deepcopy
-f1 = open("level01", "r")
-f2 = open("level02", "r")
-   
+#Task C 
 def convertRuleToArray(f):
     """
     Converts a rule to an array.
@@ -160,6 +164,9 @@ def convertRuleToArray(f):
     f.close()
     return deepcopy(array)
 
+f1 = open("level01", "r")
+f2 = open("level02", "r")
+
 arraylv1 = convertRuleToArray(f1)
 arraylv2 = convertRuleToArray(f2)
 
@@ -168,7 +175,7 @@ def writeSolutionIntoFile(path, array):
     for items in array:
         for item in items:
             if items.index(item) != len(items) - 1:
-                f.write(str(item) + " v ")
+                f.write(str(item) + " ^ ")
             else:
                 f.write(str(item))
         f.write("\n")
@@ -176,25 +183,23 @@ def writeSolutionIntoFile(path, array):
     
 writeSolutionIntoFile("level01sol", arraylv1)
 writeSolutionIntoFile("level02sol", arraylv2)
-
 #--------------------------------------------------
 #--------------------------------------------------
 #--------------------------------------------------
 #Task D
-
-def writeArrayIntoFile(path, array):
-    board = Glucose3()  
-    for item in array:
-        board.add_clause(item)
+def pySATSolver(path):
+    f = open(path,"w")
     
-    board.solve(assumptions = [4, 10])
-    tempArray = board.get_model()
+    array = Glucose3()
+    #Choosing lv2 as the input file
+    #SAT solver need row,column,diagonal 
+    for element in arraylv2:
+        array.add_clause(element)
     
-    f = open(path, "w")
-  
-        
-    f.write([item for item in tempArray if item >0])
-    f.write("\n")
+    array.solve(assumptions=[6,9])
+    tmpArray = array.get_model()
+    
+    f.write(str([element for element in tmpArray if element > 0]))
     f.close()
-
-writeArrayIntoFile("SolvingTaskD",arraylv2)
+   
+pySATSolver("SolvingTaskD") 
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## **PROJECT SAT approach for 8-queens problems**
 
-- Task A: Create 2 rule file level01 & level02
-- Task B: CNF clause for queen at cell[3][3]
-- Task C: Convert cnf to sentence 
+- Task A: Create 2 rule file level01 & level02 :joy:
+- Task B: CNF clause for queen at cell[3][3] :joy:
+- Task C: Convert cnf back to sentences :joy:
+- Task D: Using pySAT solver to find set of satisfied values of Queens's position :joy:
+- Task E: A* coding...
